0087dde9e078a959a394d4174d37749557646425
Cuervinho <50245754+Cuervinho@users.noreply.github.com>
2020-05-11 10:30:56 -0500

    Update Reader.py

--- Reader.py
+++ Reader.py
@@ -13,6 +13,7 @@ class Reader:
         with open(self.filename, "r", encoding="utf8") as file:
             word = ""
             Words = list(string.ascii_letters)
+            Words.extend(['á','é','í','ó','ú','ñ','Ñ'])
             self.info = []
 
             for i in file.read():
